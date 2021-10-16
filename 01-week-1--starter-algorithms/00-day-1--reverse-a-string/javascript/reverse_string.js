09c811bfecef8311593428da70abbe76bb88a488
Natalie Taktachev <ntaktach@gmail.com>
2021-10-16 14:49:26 -0400

    week1day1 - js problem

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,6 +1,12 @@
 function reverseString(str) {
   // type your code here
-}
+  let reversed = ""; 
+  for (let i = str.length -1; i > -1; --i) {
+    reversed = reversed + str[i];
+  }
+    return reversed 
+  }
+
 
 if (require.main === module) {
   // add your own tests in here
@@ -16,4 +22,5 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
-// And a written explanation of your solution
+// And a written explanation of your solution 
+//******* iterating over input string backwards and adding whichever character is currently being iterated over into the result string */
