dfb7998ece35e9b19e2c994390b39464677d760a
Natalie Taktachev <ntaktach@gmail.com>
2021-10-31 12:00:57 -0400

    big o - stack - rb

--- 06-week-5--big-o-continued/00-days-1-to-2--implement-a-stack-class/ruby/stack.rb
+++ 06-week-5--big-o-continued/00-days-1-to-2--implement-a-stack-class/ruby/stack.rb
@@ -11,35 +11,52 @@ class Stack
   # add item to top of stack if not full
   # if full, throw error
   def push(item)
+ 
+   if isFull? 
+    raise 'Stack is full' 
+   else
+    @stack.push(item)
+   end
   end
 
   # remove item from top of stack and return it
   def pop
+    return @stack.pop #.pop in ruby removes and returns last element 
   end
 
   # return item at top of stack without removing it
   def peek
+     @stack.last
   end
 
   # return true if stack is empty, otherwise false
   def isEmpty?
+    @stack.size.zero?
   end
 
   # return true if stack is full, otherwise false
   def isFull?
+    @stack.size == @limit
   end
 
   # return number of items in stack
   def size
+   @stack.length #.length & .size = same 
   end
 
   # return -1 if item not in stack, otherwise integer representing 
   # how far it is from the top
   def search(target)
-  end
+     @stack.each_with_index do |item, index| #each with index iterates & gets element as well as index 
+     return size - index - 1 if item == target 
+     end 
+      -1 
+  end 
+
 
   # print contents of stack: do not return the stack itself!
   def print
+    puts @stack.join(' , ')
   end
 end
 
